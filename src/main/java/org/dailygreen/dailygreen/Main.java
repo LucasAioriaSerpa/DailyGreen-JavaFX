183636deb15edca1a4bf1983e2f2616531577236
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-06-09 20:19:44 -0300

    css background image working and other stuff

--- src/main/java/org/dailygreen/dailygreen/Main.java
+++ src/main/java/org/dailygreen/dailygreen/Main.java
@@ -1,28 +1,30 @@
 package org.dailygreen.dailygreen;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import org.dailygreen.dailygreen.MainController;
-import org.dailygreen.dailygreen.Users.Administrador.MainAdm;
-import org.dailygreen.dailygreen.Users.Participante.Participante;
-import org.dailygreen.dailygreen.Users.Participante.ParticipanteMain;
-import org.dailygreen.dailygreen.util.CriptografiaComArquivo;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 import javax.crypto.SecretKey;
 
-import java.io.*;
+import javafx.scene.layout.*;
+import org.dailygreen.dailygreen.Users.Administrador.MainAdm;
+import org.dailygreen.dailygreen.Users.Participante.ParticipanteMain;
+import org.dailygreen.dailygreen.util.CriptografiaComArquivo;
+
+import javafx.scene.image.Image;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
         Button btnAdm = new Button("Administrador");
-        btnAdm.setOnAction(e -> {
+        btnAdm.getStyleClass().add("btn-adm");
+        btnAdm.getStyleClass().add("btn");
+        btnAdm.setOnAction(_ -> {
             try {
                 MainAdm adm = new MainAdm();
                 adm.start(stage);
@@ -32,20 +34,32 @@ public class Main extends Application {
             }
         });
         Button btnUser = new Button("Usuario");
-        btnUser.setOnAction(e -> {
-           try {
-               ParticipanteMain user = new ParticipanteMain();
-               user.start(stage);
-           } catch (IOException ex) {
-               ex.printStackTrace();
-               throw new RuntimeException(ex);
-           }
+        btnUser.getStyleClass().add("btn-user");
+        btnUser.getStyleClass().add("btn");
+        btnUser.setOnAction(_ -> {
+            try {
+                ParticipanteMain user = new ParticipanteMain();
+                user.start(stage);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            }
         });
-        VBox root = new VBox(10, btnAdm, btnUser); root.setStyle("" +
-                "-fx-padding: 10; " +
-                "-fx-alignment: center;" +
-                "");
+
+        Image image = new Image(getClass().getResource("/IMAGES/BACKGROUNDS/florest-1.jpeg").toExternalForm());
+        BackgroundImage bg = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        VBox root = new VBox();
+        root.getStyleClass().add("root");
+        root.setBackground(new Background(bg));
+
+        VBox btnsBox = new VBox(10, btnAdm, btnUser);
+        btnsBox.getStyleClass().add("btns");
+
+        root.getChildren().add(btnsBox);
         Scene scene = new Scene(root, 800, 500);
+        scene.getStylesheets()
+                .add(Objects.requireNonNull(getClass().getResource("/CSS/classMain.css")).toExternalForm());
         stage.setTitle("MAIN-PAGE");
         stage.setScene(scene);
         stage.show();
